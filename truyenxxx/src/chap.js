a8c82af2f598836879c97b4545e49488daab096b
VietVu <vietvu1398@gmail.com>
2026-03-11 19:43:25 +0700

    sua lai chap.js

--- truyenxxx/src/chap.js
+++ truyenxxx/src/chap.js
@@ -1,21 +1,9 @@
-load("libs.js");
-load("config.js");
-
 function execute(url) {
-  url = url.replace(
-    /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/gim,
-    BASE_URL
-  );
-  var response = fetch(url);
-  if (response.ok) {
-    var doc = response.html();
-    var htm = $.Q(doc, ".ndtruyen", {
-      remove: 'em, a[target="_blank"]',
-    }).html();
-    htm = htm.replace(/<br>|\\n/g, "<br><br>");
-    return Response.success(htm);
-  }
-  return null;
+    let response = fetch(url);
+    if (response.ok) {
+        let doc = response.html();
+        let txt = doc.select(".single-content").html();
+        return Response.success(txt);
+    }
+    return null;
 }
-
-//em, center, a[target="_blank"]
